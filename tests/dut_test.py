ae215a0965efb163b139886af6233a067274cf16
DhavalCoder <149752141+DhavalCoder@users.noreply.github.com>
2025-05-02 23:59:15 +0530

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -4,4 +4,10 @@ from cocotb.triggers import Timer
 
 @cocotb.test()
 async def dut_test(dut):
-    assert 1, "Test Not Implemented Error"
+    dut.inp.value = 1         
+    await Timer(10, units="ns")
+    assert dut.out.value == 1 
+initial begin
+    $dumpfile("tests/dut.vcd");    
+    $dumpvars(0, dut);    
+end
