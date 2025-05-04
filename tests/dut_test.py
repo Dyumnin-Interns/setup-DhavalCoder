649ce21d56c435d6f48a6b3e44c892b0556095d6
DhavalCoder <149752141+DhavalCoder@users.noreply.github.com>
2025-05-04 07:40:16 +0530

    Update dut_test.py

--- tests/dut_test.py
+++ tests/dut_test.py
@@ -1,18 +1,16 @@
+
 import cocotb
 from cocotb.triggers import Timer
-from random import random as rand
 
-def xor(a, b):
-    return a^b
 
 @cocotb.test()
 async def dut_test(dut):
-    dut.a.value = 0
-    dut.b.value = 0
-    await Timer(1, 'ns')
-    assert dut.y.value == 0, "Simple Test"
-    for i in range(10):
-        dut.a.value = 0 if rand() < 0.5 else 1
-        dut.b.value = 0 if rand() < 0.5 else 1
-        await Timer(1, 'ns')
-        assert dut.y.value == xor(dut.a.value, dut.b.value), f"Random Test {i+1}"
+    a = (0,0,1,1)
+    b = (0,1,0,1)
+    y = (0,1,1,0)
+
+    for i in range(4):
+            dut.a.value = a[i]
+            dut.b.value = b[i]
+            await Timer(1,'ns')
+            assert dut.y.value == y[i], f"Error at iteration"
